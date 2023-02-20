7dffb51e482d0786ce6a60062c94eee22a606de1
Pablo Misael Peloc <pmpeloc@gmail.com>
2023-02-20 15:27:12 -0300

    feat: Repositories added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from './infrastructure/logger/logger.module';
 import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
+import { RepositoriesModule } from './infrastructure/repositories/repositories.module';
 
 @Module({
-  imports: [LoggerModule, ExceptionsModule],
+  imports: [LoggerModule, ExceptionsModule, RepositoriesModule],
   controllers: [],
   providers: [],
 })
