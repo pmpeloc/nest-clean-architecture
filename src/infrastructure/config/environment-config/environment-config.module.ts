46e5ed6db84f309b8fd7f0405ae29b8f961a8410
Pablo Misael Peloc <pmpeloc@gmail.com>
2023-02-20 15:07:25 -0300

    feat: TypeORM added

--- src/infrastructure/config/environment-config/environment-config.module.ts
+++ src/infrastructure/config/environment-config/environment-config.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { EnvironmentConfigService } from './environment-config.service';
 
 @Module({
-  providers: [EnvironmentConfigService]
+  providers: [EnvironmentConfigService],
+  exports: [EnvironmentConfigService],
 })
 export class EnvironmentConfigModule {}
